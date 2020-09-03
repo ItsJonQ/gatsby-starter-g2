c6fc285e1917082fc9eb4fd4c0580083f8e893c3
sixhours <calobee@gmail.com>
2020-09-03 15:13:09 -0400

    Improvements to single plugin view to better align with newer mockups

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Spacer, Separator, Heading, HStack, Text, Elevation, View, VStack, Card, Placeholder, Grid, CardBody, Button } from "@wp-g2/components"
+import { Spacer, Separator, Icon, Heading, HStack, Text, Elevation, View, VStack, Card, Placeholder, Grid, CardBody, Button } from "@wp-g2/components"
+import { FiStar } from '@wp-g2/icons'
 
 export function PluginCard( { title } ) {
 	return (
-		<Card p={ 3 } m={ 3 }>
-			<CardBody>
-					<Grid columns={ 2 } templateColumns={ "50px auto" } align={ "start" } gap={ "8px" }>
-						<Placeholder width={ 50 } height={ 50 } css={ "border-radius: 50%" } />
-						<VStack>
+		<Card m={ 3 }>
+			<CardBody css={ "padding: 0" }>
+					<Grid columns={ 2 } templateColumns={ "50px auto" } align={ "start" } gap={ "0px" }>
+						<Placeholder width={ 50 } height={ 50 } css={ "border-radius: 50%; margin: 12px 12px 0" } />
+						<VStack css={ "padding: 12px 12px 6px 24px" }>
 							<Heading as="h3" size={ 3 }>{ title }</Heading>
-							<Text>Description goes here.</Text>
+							<Text>by Author Name</Text>
 						</VStack>
 						<Separator my={ 0 } css={ "grid-column: 1 / span 2" } />
-						<HStack css={ "grid-column: 1 / span 2" }>
-							<Text>Reviews</Text>
+						<HStack css={ "grid-column: 1 / span 2; padding: 0 12px 12px" } spacing={ 2 }>
+							<Text css={ "margin-right: 8px" }>800k</Text>
+							<Spacer>
+								<HStack justify={ "start" } spacing={ 1 }>
+									<Icon icon={ <FiStar/> } size={ 12 } />
+									<Text>4.3</Text>
+								</HStack>
+							</Spacer>
 							<Button as={ Link } to="/plugin">Install</Button>
 						</HStack>
 					</Grid>
--- src/pages/plugin.js
+++ src/pages/plugin.js
@@ -1,67 +1,73 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Separator, Button, Alert, Alerts, Icon, Heading, Grid, Text, HStack, VStack, Card, CardBody, Placeholder, View, Spacer } from '@wp-g2/components'
+import { Separator, Tabs, Tab, TabList, TabPanel, Button, Alert, Alerts, Icon, Heading, Grid, Text, HStack, VStack, Card, CardBody, Placeholder, View, Spacer } from '@wp-g2/components'
 import { ui } from "@wp-g2/styles"
 import { FiArrowLeft } from '@wp-g2/icons'
 import { Layout } from "../core"
 
 function InfoCard( { title } ) {
-  return (
-    <Card>
-      <CardBody css={ "height: 100%" }>
-        <Grid columns={ 2 }>
-          <VStack>
-            <Heading size={2}>{ title }</Heading>
-            <Text>Description goes here.</Text>
-            <Spacer />
-            <View>Expand section</View>
-          </VStack>
-          <Placeholder width={ 500 } height={ 200 }/>
-        </Grid>
-      </CardBody>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardBody css={ "height: 100%; padding: 16px" }>
+				<VStack spacing={ 5 }>
+					<Heading size={ 3 }>{ title }</Heading>
+					<Placeholder height={ 200 } />
+					<Text>Description goes here.</Text>
+					<Spacer />
+				</VStack>
+			</CardBody>
+		</Card>
+	);
 }
 
 function Page() {
-  return (
-    <Layout>
-      <View css={ "max-width: 900px; margin: 36px auto;" }>
-        <Spacer>
-          <HStack spacing={ 5 } alignment="center">
-            <Link to="/">
-              <Icon icon={ <FiArrowLeft /> } />
-            </Link>
-            <Spacer>
-              <Alerts>
-                <Alert status="success">
-                  <Text>Your plugin has been activated</Text>
-                </Alert>
-              </Alerts>
-            </Spacer>
-          </HStack>
-          <Spacer py={ 10 }>
-            <Heading align="center">Plugin name</Heading>
-          </Spacer>
-          <HStack>
-            <Text>Plugin creator</Text>
-            <HStack>
-              <Button>Activated</Button>
-              <Button variant="primary">Edit</Button>
-            </HStack>
-          </HStack>
-        </Spacer>
-        <Separator />
-        <View css="max-width: 900px; margin: 0 auto;">
-          <VStack spacing={ 5 }>
-            <InfoCard title="Description" />
-            <InfoCard title="Support" />
-            <InfoCard title="Reviews" />
-          </VStack>
-        </View>
-      </View>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<View css={ "max-width: 900px; margin: 36px auto;" }>
+				<Spacer my={ 5 }>
+					<HStack spacing={ 5 } alignment="center">
+						<Link to="/">
+							<Icon icon={ <FiArrowLeft /> } />
+						</Link>
+						<Spacer>
+							<Alerts>
+								<Alert status="success">
+									<Text>Your plugin has been activated</Text>
+								</Alert>
+							</Alerts>
+						</Spacer>
+					</HStack>
+				</Spacer>
+				<Spacer my={ 5 }>
+					<Card>
+						<CardBody css={ "padding: 0" }>
+							<Placeholder width={ "100%" } height={ "300px" } />
+							<HStack css={ "padding: 16px" } spacing={ 4 }>
+								<Placeholder width={ "50px" } height={ "50px" } css={ "border-radius: 50%" } />
+								<Spacer>
+									<VStack>
+										<Heading size={ 2 }>Plugin name</Heading>
+										<Text>by <Link to="/">Plugin author</Link></Text>
+									</VStack>
+								</Spacer>
+								<Button variant= { "primary" }>Install</Button>
+							</HStack>
+						</CardBody>
+					</Card>
+				</Spacer>
+				<Tabs>
+					<TabList css={ "width: 33%" }>
+						<Tab>Description</Tab>
+						<Tab>Reviews</Tab>
+						<Tab>Support</Tab>
+					</TabList>
+					<TabPanel><InfoCard title="Description" /></TabPanel>
+					<TabPanel><InfoCard title="Reviews" /></TabPanel>
+					<TabPanel><InfoCard title="Support" /></TabPanel>
+				</Tabs>
+			</View>
+		</Layout>
+	)
 }
 
 export default Page
