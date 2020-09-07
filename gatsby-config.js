59ce7af3b0f6edc7c8a756a6ab0f8d152530f450
Jon Q <hello@jonquach.com>
2020-09-07 14:01:15 -0400

    Remove styled-components Gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,5 @@ module.exports = {
     description: `Kick off your next, great prototype with this G2 starter.`,
     author: `@itsjonq`,
   },
-  plugins: [`@wp-g2/gatsby-plugin-styles`, `gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
+  plugins: [`@wp-g2/gatsby-plugin-styles`, `gatsby-plugin-react-helmet`],
 }
